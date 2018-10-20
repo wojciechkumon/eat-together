32541641b0c738c870619af3670ed3ba354bf2cd
Przemyslaw Fedorowiat <p.fedorowiat@gmail.com>
2018-10-20 20:25:02 +0200

    added MealItem

---
+++ frontend/src/components/MealItem.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import {Meal} from "./MealInterface";
+import {intolerancesTypes} from "./intolerancesTypes";
+
+class MealItem extends React.PureComponent<MealItemProps> {
+
+  render() {
+    const {meal} = this.props;
+    return (
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title text-et">{meal.name}</h5>
+          <div>
+            {meal.ingredients.split(',').map(ingredient =>
+              <span className="badge badge-dark">{ingredient}</span>
+            )}
+          </div>
+          <div>
+            {meal.intolerances.map(intolerance =>
+              intolerancesTypes.filter(x => x.name === intolerance).map(intoleranceType =>
+                <button type="button" className="btn btn-outline-secondary"><span
+                  style={{textDecoration: 'line-through'}}>{intoleranceType.icon}</span> {intoleranceType.text}</button>
+              )
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+interface MealItemProps {
+  meal: Meal;
+}
+
+export default MealItem;
