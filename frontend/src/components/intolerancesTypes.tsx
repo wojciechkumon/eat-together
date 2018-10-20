5131dbe2100c2ff4f45696995e7d13f4d51b7017
Przemyslaw Fedorowiat <p.fedorowiat@gmail.com>
2018-10-20 20:03:30 +0200

    added intoleranceTypes map

---
+++ frontend/src/components/intolerancesTypes.tsx
@@ -0,0 +1,6 @@
+export const intolerancesTypes = [
+  {name: 'LACTO', text: 'Lactose Free', icon: '🐄'},
+  {name: 'GLUTEN', text: 'Gluten Free', icon: '🌾'},
+  {name: 'NUTS', text: 'Nuts Free', icon: '🌰'},
+  {name: 'FISH', text: 'No Fish', icon: '🐟'}
+];
