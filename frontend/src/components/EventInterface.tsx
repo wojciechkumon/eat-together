858c64383f68bddd441b276855f7dd744aee0ffe
Przemyslaw Fedorowiat <p.fedorowiat@gmail.com>
2018-10-20 18:38:23 +0200

    event and meal interfaces

---
+++ frontend/src/components/EventInterface.tsx
@@ -0,0 +1,18 @@
+import {Meal} from "./MealInterface";
+
+export interface Event {
+  name: string;
+  description: string;
+  estimatedPrice: number;
+  currency: string;
+  maxParticipants: number;
+  cuisines: string[];
+  dateTime: string;
+  streetWithNumber: string;
+  city: string;
+  phoneNumber: string;
+  zip: string;
+  country: string;
+  meals: Meal[]
+}
+
---
+++ frontend/src/components/MealInterface.tsx
@@ -0,0 +1,5 @@
+export interface Meal {
+  name: string;
+  ingredients: string[];
+  intolerances: string[];
+}
