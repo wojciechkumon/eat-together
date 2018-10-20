b71b05afba92bb58c15698c30f454d7aea0b2cf3
Gaba Bartnicka <bartnicka.g@gmail.com>
2018-10-20 11:42:10 +0200

    init project

---
+++ src/main/java/com/blackheronteam/EatTogether/EatTogetherApplication.java
@@ -0,0 +1,12 @@
+package com.blackheronteam.EatTogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatTogetherApplication.class, args);
+	}
+}
