5ef53c90d30e0abf0d48c4175a3fab213cecad2e
Gaba Bartnicka <bartnicka.g@gmail.com>
2018-10-20 12:01:14 +0200

    user domain object

---
+++ src/main/java/com/blackheronteam/EatTogether/domain/User.java
@@ -0,0 +1,54 @@
+package com.blackheronteam.EatTogether.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+public class User implements UserDetails {
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
