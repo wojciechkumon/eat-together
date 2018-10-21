075ef05fe5b7275c70de6d13d4e7e6c7b3d1440e
Wojciech Kumoń <wojciechkumon@gmail.com>
2018-10-21 03:50:03 +0200

    event fix

--- src/main/java/com/blackheronteam/EatTogether/domain/Event.java
+++ src/main/java/com/blackheronteam/EatTogether/domain/Event.java
@@ -7,6 +7,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import java.time.LocalDateTime;
